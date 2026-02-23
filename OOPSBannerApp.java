f5ba1f45faa61db497050f62cdd997b54e143541
Ashika M <majiashika@gmail.com>
2026-02-23 10:43:35 +0530

    UC4: OOPS Banner using String Array and Loop

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * 
+ * This class demonstates a simple Java application that displays the Object Oriented Programming System OOPS acronym to the console. 
+ * UC1: Print OOPS to Console
+ * 
+ * @author Ashika
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
---
+++ UC2.java
@@ -0,0 +1,44 @@
+public class UC2 {
+
+
+    public static void main(String[] args) {
+
+        // Each letter is 9 characters wide, 7 lines tall
+
+        // Line 1
+        System.out.println(
+            "  *****   " + "  *****   " + " ******** " + "  *****   "
+        );
+
+        // Line 2
+        System.out.println(
+            " *     *  " + " *     *  " + " *       " + " *     *  "
+        );
+
+        // Line 3
+        System.out.println(
+            "*       * " + "*       * " + " *       " + "*          "
+        );
+
+        // Line 4
+        System.out.println(
+            "*       * " + "*       * " + " ******** " + "  *****   "
+        );
+
+        // Line 5
+        System.out.println(
+            "*       * " + "*       * " + " *       " + "       *  "
+        );
+
+        // Line 6
+        System.out.println(
+            " *     *  " + " *     *  " + " *       " + " *     *  "
+        );
+
+        // Line 7
+        System.out.println(
+            "  *****   " + "  *****   " + " *        " + "  *****   "
+        );
+    }
+}
+
